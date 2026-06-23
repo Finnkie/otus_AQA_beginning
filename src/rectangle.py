da9b9de512538ea72851e6a8f38371f8a4e82c96
igradusov <igradusov@astralinux.ru>
2026-06-24 02:19:17 +0300

    Added project files + new test

--- src/rectangle.py
+++ src/rectangle.py
@@ -1,4 +1,4 @@
-from figure import Figure 
+from src.figure import Figure
 
 
 class Rectangle(Figure):
@@ -6,9 +6,9 @@ class Rectangle(Figure):
     def __init__(self, side_a: int | float, side_b: int | float):
         self.side_a, self.side_b = side_a, side_b
         if not isinstance(side_a, (int, float)) or not isinstance (side_b, (int, float)):
-            raise TypeError('\n\nSides parameters must be type: "int" or "float"')
+            raise TypeError('Side parameters types must be: "int" or "float"')
         if side_a <= 0 or side_b <= 0:
-            raise ValueError(f'\n\nSide parameter values must be above zero.\nYour values: side_a: {side_a}\tside_b: {side_b}')
+            raise ValueError(f'Side parameters must be above zero.\nYour values: side_a: {side_a}\tside_b: {side_b}')
 
 
     @property
