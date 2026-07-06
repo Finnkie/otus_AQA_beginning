1ef0821e916dcf5d8f105214efb1f9ab656a0918
igradusov <igradusov@astralinux.ru>
2026-07-06 16:32:31 +0300

    Adjusted script

--- script_data_convert.py
+++ script_data_convert.py
@@ -2,8 +2,8 @@ import json
 import csv
 from typing import List, Dict, Union
 from pathlib import Path
-from src import *
 
+BASE_DIR = Path(__file__).resolve().parent
 
 def read_table_csv(filename: str) -> List[Dict]:
     file_path = BASE_DIR / "src" / filename
@@ -14,27 +14,30 @@ def read_table_csv(filename: str) -> List[Dict]:
 
 def read_json(filename: str) -> Union[list, dict]:
     file_path = BASE_DIR / "src" / filename
-    with open(file_path, 'r', encoding='utf=8') as file:
+    with open(file_path, 'r', encoding='utf-8') as file:
         return json.load(file)
 
 
 def create_json(filename: str, data: Union[list, dict]) -> None:
     file_path = BASE_DIR / "src" / filename
-    with open(file_path, 'w', encoding='utf=8') as file:
-        return json.dump(data, file, indent = 4, ensure_ascii = False)
+    with open(file_path, 'w', encoding='utf-8') as file:
+        json.dump(data, file, indent=4, ensure_ascii=False)
 
 
-user_list = [{"name": user["name"], "gender": user["gender"], "address": user["address"], "age": user["age"], "books": []} for user in read_json("users.json")]
-book_list = [{"title": book["Title"], "author": book["Author"], "pages": book["Pages"], "genre": book["Genre"]} for book in read_table_csv("books.csv")]
+def create_result(output_filename: str) -> None:
+    user_list = [{"name": user["name"], "gender": user["gender"], "address": user["address"], "age": user["age"], "books": []} for user in read_json("users.json")]
+    book_list = [{"title": book["Title"], "author": book["Author"], "pages": book["Pages"], "genre": book["Genre"]} for book in read_table_csv("books.csv")]
+    
+    book_index = 0
+    while book_index < len(book_list):
+        for user in user_list:
+            if book_index < len(book_list):
+                user["books"].append(book_list[book_index])
+                book_index += 1
+            else:
+                break
+    
+    create_json(output_filename, user_list)
 
-book_index = 0
-while book_index < len(book_list):
-    for user in user_list:
-        if book_index < len(book_list):
-            user["books"].append(book_list[book_index])
-            book_index += 1
-        else:
-            break
 
-create_json("result.json", user_list)
-
+create_result("result.json")
